8e5090b479339dcf9cb4c5e5044dadd6d3a06c89
김상준-재택 <sang1009@naver.com>
2023-07-13 11:45:23 +0900

    mmm

---
+++ react-nodejs-sqlite/server/server.js
@@ -0,0 +1,36 @@
+// server.js
+
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const database = require('./database');
+
+const app = express();
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      age INTEGER
+    )
+  `);
+
+  db.run("INSERT INTO users (name, age) VALUES (?, ?)", ['John Doe', 30]);
+  db.run("INSERT INTO users (name, age) VALUES (?, ?)", ['Jane Smith', 25]);
+});
+
+app.get('/api/users', (req, res) => {
+  db.all("SELECT * FROM users", (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
